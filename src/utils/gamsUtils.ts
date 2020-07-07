58ff006088d896ade4bd6af228d778c3ca62b0c1
Sebi <SebastianStoff@gmx.at>
2020-07-07 11:11:11 +0200

    added copy deep to gamsUtils

--- src/utils/gamsUtils.ts
+++ src/utils/gamsUtils.ts
@@ -75,3 +75,13 @@ export const setRestParam = (paramName: string, value: string): void => {
   url.searchParams.set(paramName, value);
   window.location.href = url.href;
 }
+
+/**
+ * Uses JSON.parse and JSON.stringify to generate 
+ * a deep copy of given value.
+ * @param {Object | Array<any>} value Object or Array to copy deep.
+ * @returns {Object | Array<any>} deep copy of given Object or Array.
+ */
+export const copyDeep = (value: Object | Array<any>) => {
+  return JSON.parse(JSON.stringify(value));
+}
